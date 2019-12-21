d7c64992205a8fc7d08423b454e2a6dbc2c07957
Badala96 <Badala96@github.com>
2019-12-21 17:29:52 +0600

    changes

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/Collaboration-Hackathon/';
+$config['base_url'] = 'http://localhost/CollaborationManagement/hackathon';
 
 $config['index_page'] = 'index.php';
 
